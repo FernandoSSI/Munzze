d58e65e012f1c03156404c832205e2a2871378c1
FernandoSSI <fernandosi2005@gmail.com>
2024-02-06 23:21:02 -0300

    Initial commit

---
+++ src/main/java/github/FernandoSSI/Munzze/MunzzeApplication.java
@@ -0,0 +1,13 @@
+package github.FernandoSSI.Munzze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunzzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MunzzeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/FernandoSSI/Munzze/MunzzeApplicationTests.java
@@ -0,0 +1,13 @@
+package github.FernandoSSI.Munzze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunzzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
