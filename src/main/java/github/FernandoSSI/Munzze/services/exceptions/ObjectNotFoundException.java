f60a25208d05df9fdb95a0b82de0934ec3768925
FernandoSSI <fernandosi2005@gmail.com>
2024-02-08 19:26:24 -0300

    Create ObjectNotFoundException.java

---
+++ src/main/java/github/FernandoSSI/Munzze/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package github.FernandoSSI.Munzze.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+}
